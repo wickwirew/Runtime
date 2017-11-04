9f09cca1a0113b41b3a72abdc46e5992981cf8ad
Wesley Wickwire <wickwirew@gmail.com>
2017-11-04 10:13:25 -0400

    moved ntd

--- RuntimeTests/FactoryTests.swift
+++ RuntimeTests/FactoryTests.swift
@@ -23,7 +23,7 @@ class FactoryTests: XCTestCase {
     }
     
     func testClass() throws {
-        var person: PersonClass = try build()
+        let person: PersonClass = try build()
         XCTAssert(person.firstname == "")
         XCTAssert(person.lastname == "")
         XCTAssert(person.age == 0)
