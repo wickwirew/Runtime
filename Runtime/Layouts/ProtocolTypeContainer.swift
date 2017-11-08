e133cbd7f5110b7e31fd355250a12ebf8812cb25
Wesley Wickwire <wickwirew@gmail.com>
2017-11-07 19:49:27 -0500

    removed unused files.

--- Runtime/Layouts/ProtocolTypeContainer.swift
+++ Runtime/Layouts/ProtocolTypeContainer.swift
@@ -23,29 +23,8 @@
 import Foundation
 
 
-protocol Getters {}
-extension Getters {
-    static func get(from pointer: UnsafeRawPointer) -> Any {
-        return pointer.assumingMemoryBound(to: self).pointee
-    }
-}
 
-func getters(type: Any.Type) -> Getters.Type {
-    let container = ProtocolTypeContainer(type: type, witnessTable: 0)
-    return unsafeBitCast(container, to: Getters.Type.self)
-}
-
-
-protocol Setters {}
-extension Setters {
-    static func set(value: Any, pointer: UnsafeMutableRawPointer) {
-        if let value = value as? Self {
-            pointer.assumingMemoryBound(to: self).initialize(to: value)
-        }
-    }
-}
-
-func setters(type: Any.Type) -> Setters.Type {
-    let container = ProtocolTypeContainer(type: type, witnessTable: 0)
-    return unsafeBitCast(container, to: Setters.Type.self)
+struct ProtocolTypeContainer {
+    let type: Any.Type
+    let witnessTable: Int
 }
