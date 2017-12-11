11e65415648a0858a8664148313c90c4d5ced4c8
Helge Heß <devteam@zeezide.de>
2017-12-11 14:12:00 +0100

    Drop comment, be xplicit about the version
    ... tied it to Swift 4, doesn't work for 3, right?

--- Package.swift
+++ Package.swift
@@ -11,13 +11,12 @@ let package = Package(
     ],
     dependencies: [],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Runtime",
             dependencies: []),
         .testTarget(
             name: "RuntimeTests",
             dependencies: ["Runtime"]),
-    ]
+    ],
+    swiftLanguageVersions: [4]
 )
