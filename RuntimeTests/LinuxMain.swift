9f9e3078fff093adbd8f43df1052ee5d68b05c2a
Wesley Wickwire <wickwirew@gmail.com>
2018-04-07 16:38:29 -0400

    upped version number

---
+++ RuntimeTests/LinuxMain.swift
@@ -0,0 +1,12 @@
+// This file is generated. Please do not add to source control.
+@testable import Runtime
+import XCTest
+
+XCTMain([ 
+   testCase(FactoryTests.allTests), 
+   testCase(GetSetClassTests.allTests), 
+   testCase(GetSetStructTests.allTests), 
+   testCase(MetadataTests.allTests), 
+   testCase(ValuePointerTests.allTests), 
+   testCase(ValueWitnessTableTests.allTests),
+])
