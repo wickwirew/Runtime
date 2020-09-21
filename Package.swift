988cd0b06ce26bfded83bd59dfdcdb5f9d39c0dc
Jason Jobe <box2019@jasonjobe.com>
2020-09-21 19:54:49 -0400

    added CRuntime to products

--- Package.swift
+++ Package.swift
@@ -1,11 +1,16 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
+
 let package = Package(
     name: "Runtime",
+    platforms: [
+        .macOS(.v10_15),
+        .iOS(.v13), .tvOS(.v13),
+    ],
     products: [
-        .library(
-            name: "Runtime",
-            targets: ["Runtime"])
+        .library(name: "Runtime", targets: ["Runtime"]),
+        .library(name: "CRuntime", targets: ["CRuntime"])
         ],
     targets: [
         .target(
